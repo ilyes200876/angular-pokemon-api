fb78182c9279d73f2c02cbc831d61872bf0975d2
ilyes200876 <ilyes200876@live.fr>
2023-09-11 16:24:58 +0200

    show pokemon list from api

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable} from '@angular/core';
+import { PokemonInterface } from './pokemon-interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchAllPokemon(): Observable<PokemonInterface[]>{
+    return this.http.get<PokemonInterface[]>('http://vps204.tyrolium.fr/apiPokemon/index.php?controller=pokemon&task=getAll');
+  }
+
 }
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { PokemonInterface } from '../pokemon-interface';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent {
+export class PokemonComponent implements OnInit{
+
+  pokemons: PokemonInterface[]|undefined;
+
+  constructor(private pokemonService: PokemonService){}
+
+  ngOnInit(){
+
+    this.pokemonService.fetchAllPokemon().subscribe(pokemonTronslate => [
+      this.pokemons = pokemonTronslate
+    ]);
+  }
 
 }
